63dbba46ca31a3555817e74c0567e76941fa2baa
tofu345 <shammahayotunde1@gmail.com>
2024-05-29 19:07:07 +0100

    user store in local storage

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,6 +1,16 @@
 import type { User } from "$lib/types";
 import { writable, type Writable } from "svelte/store";
+import { browser } from "$app/environment";
 
-const user: Writable<User | null> = writable(null);
+const key = "user";
+const user: Writable<User | null> = writable(
+    (browser && (JSON.parse(localStorage.getItem(key) || "null") as User)) || null,
+);
+
+user.subscribe((value) => {
+    if (browser) {
+        return localStorage.setItem(key, JSON.stringify(value));
+    }
+});
 
 export default user;
